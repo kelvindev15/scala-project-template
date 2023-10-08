ac8c0be0638c103d174f0f13dec46a7e15370e12
kelvin-olaiya <73821880+kelvin-olaiya@users.noreply.github.com>
2023-10-08 21:56:34 +0000

    Deploying to gh-pages from @ kelvin-olaiya/scala-project-template@f91e7f969cdeba272c68f3329f181a4fb8956256 🚀

---
+++ api/scripts/searchData.js
@@ -0,0 +1,6 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"Hello","t":"Hello","d":"","k":"static","x":""},
+{"l":"example.html#","e":false,"i":"","n":"example","t":"example","d":"","k":"package","x":""},
+{"l":"example/Hello$.html#","e":false,"i":"","n":"Hello","t":"Hello","d":"example","k":"object","x":""},
+{"l":"example/Hello$.html#main-913","e":false,"i":"","n":"main","t":"main(args: Array[String]): Unit","d":"example.Hello","k":"def","x":""},
+{"l":"example/IsItFriday$.html#","e":false,"i":"","n":"IsItFriday","t":"IsItFriday","d":"example","k":"object","x":""},
+{"l":"example/IsItFriday$.html#isItFriday-fffff2ca","e":false,"i":"","n":"isItFriday","t":"isItFriday(today: String): String","d":"example.IsItFriday","k":"def","x":""}];
