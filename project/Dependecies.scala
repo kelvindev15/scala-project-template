4b3c674838cfdcd683052f92654951a2040255ac
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-03-15 09:38:46 +0100

    build(deps): use typesafe test classpath setting

--- project/Dependecies.scala
+++ project/Dependecies.scala
@@ -9,18 +9,18 @@ object Dependecies {
   /*
    * Libraries
    */
-  val cucumberCore = "io.cucumber" % "cucumber-core" % "7.15.0" % "test"
+  val cucumberCore = "io.cucumber" % "cucumber-core" % "7.15.0" % Test
   val cucumberJunit =
-    "io.cucumber" % "cucumber-junit" % cucumberVersion % "test"
-  val cucumberJvm = "io.cucumber" % "cucumber-jvm" % cucumberVersion % "test"
+    "io.cucumber" % "cucumber-junit" % cucumberVersion % Test
+  val cucumberJvm = "io.cucumber" % "cucumber-jvm" % cucumberVersion % Test
   val cucumberScala = "io.cucumber" %% "cucumber-scala" % cucumberVersion % Test
   val scalactic = "org.scalactic" %% "scalactic" % scalaTestVersion
-  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0" % Test
   val scalaTestJUnit5 =
-    "org.scalatestplus" %% "junit-5-10" % "3.2.17.0" % "test"
+    "org.scalatestplus" %% "junit-5-10" % "3.2.17.0" % Test
   val scalaTestMockito =
-    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % "test"
+    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % Test
   /*
    * Bundles
    */
